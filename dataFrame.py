f30867cc3f09c53648e0404ed52eee98a46a24f9
armani <armanizuniga7@gmail.com>
2025-04-12 19:42:57 -0500

    Added generate jobs function

--- dataFrame.py
+++ dataFrame.py
@@ -3,17 +3,28 @@ import randomName
 import numpy as np
 
 
+# Function to generate job roles and types then appends to the existing dataFrame
+def generate_jobs(df):
+    # Define job roles and full-time/part-time distribution
+    jobs = ["Genius"] * 10 + ["Technical Expert"] * 10 + ["Technical Specialist"] * 20
+    job_type = ["FT"] * 17 + ["PT"] * 23
+
+    # Randomize the order
+    np.random.shuffle(jobs)
+    np.random.shuffle(job_type)
+
+    # Add new columns to the existing dataFrame
+    df["Jobs"] = jobs
+    df["Type"] = job_type
+
+
 def create_data():
     # Create DataFrame with a column "Name"
     df = pd.DataFrame({"Name": randomName.generate_names()})
-
-    # Define job distribution
-    jobs = ["Genius"] * 10 + ["Technical Expert"] * 10 + ["Technical Specialist"] * 20
-    np.random.shuffle(jobs)  # Randomize the order
-    df["Jobs"] = jobs
-    df["Type"] = ["FT"] * 17 + ["PT"] * 23
-    df = df[["Jobs", "Type", "Name"]]  # Reorder columnes
+    generate_jobs(df)
     # Display the DataFrame
+    # Reorder columns for organization
+    df = df[["Jobs", "Type", "Name"]]
     print(df)
 
 
